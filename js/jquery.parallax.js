bb2d3e8d91e47cf39b51e963183cb027afb9c39f
paravozz <prvz-work@outlook.com>
2015-06-01 19:50:08 +0300

    Revert "Less fly"
    This reverts commit 07a2f2180afa02baa703de1b675c1fef8cf64652.

--- js/jquery.parallax.js
+++ js/jquery.parallax.js
@@ -18,7 +18,7 @@
 	    	yparallax:     true,    //
 	    	xorigin:       0.5,     // 0-1 - Sets default alignment. Only has effect when parallax values are something other than 1 (or true, or '100%')
 	    	yorigin:       0.5,     //
-	    	decay:         0.9,    // 0-1 (0 instant, 1 forever) - Sets rate of decay curve for catching up with target mouse position
+	    	decay:         0.66,    // 0-1 (0 instant, 1 forever) - Sets rate of decay curve for catching up with target mouse position
 	    	frameDuration: 30,      // Int (milliseconds)
 	    	freezeClass:   'freeze' // String - Class added to layer when frozen
 	    },
